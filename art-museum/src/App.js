1bf63aeafc7b339fcd0792ec879e5915818f4f48
govighan16 <govighan@gmail.com>
2023-09-07 20:21:16 +0100

    Home and not found page

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -1,15 +1,24 @@
 import harvardArt from "./data/harvardArt";
 import GalleryNavigation from "./components/GalleryNavigation";
 import GalleryView from './components/GalleryView';
-import {Route} from 'react-router-dom'
+import Home from './components/Home';
+import {Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className = 'pageWrapper'>
     <GalleryNavigation galleries = {harvardArt.records}/>
+    <Switch>
+    <Route exact path = '/'>
+      <Home />
+    </Route>
     <Route path = '/galleries/:galleryId'>
       <GalleryView galleries={harvardArt.records}/>
     </Route>
+    <Route>
+        <h2>Page Not Found :/</h2>
+    </Route>
+    </Switch>
     </div>
   );
 }
---
+++ art-museum/src/components/Home/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+        <h2>Havard Art Museum</h2>
+        <p>Look, but don't touch. Please select a Gallery in the Navigation Bar.</p>
+        </div>
+    )
+}
+
+export default Home;
