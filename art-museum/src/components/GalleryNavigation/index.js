f6cf9bab79a73da76bcdf0ced53179356457c338
govighan16 <govighan@gmail.com>
2023-09-07 19:25:07 +0100

    Starter files and basic galleries

---
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,16 @@
+import {NavLink} from 'react-router-dom';
+import './GalleryNavigation.css'
+
+function GalleryNavigation({galleries}) {
+    return (
+    <nav>
+        <h1> Galleries </h1>
+        <NavLink to = '/'>Home</NavLink>
+        {galleries.map(gallery => {
+            return <NavLink key={gallery.gallerynumber} to = {`/galleries/${gallery.gallerynumber}`}> {gallery.name} </NavLink>
+        })}
+    </nav>
+    )
+}
+
+export default GalleryNavigation;
